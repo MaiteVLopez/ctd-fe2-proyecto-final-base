9fdca69f5e915140a0690b3c3afc94b9f889a60c
Alejo <alejoivanrojas@gmail.com>
2022-07-09 23:08:06 -0300

    test fixed

--- src/mocks/cita.mock.tsx
+++ src/mocks/cita.mock.tsx
@@ -1,12 +1,11 @@
 import { rest } from "msw";
 import { API_URL } from "../app/constants";
-import { ICita } from "../features/quote/types";
 export const generateHandlers = () => {
-  const  data : ICita = {
-    personaje: 'Apu Nahasapeemapetilon',
-    cita: 'Thank you. Come again.',
-    imagen:'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629',
-    direccionPersonaje:'Left'},
+  const  data = {
+    character: 'Apu Nahasapeemapetilon',
+    quote: 'Thank you. Come again.',
+    image:'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629',
+    characterDirection:'Left'},
  
     handlers = [
       rest.get(API_URL, (req, res, ctx) => {
